ab22a213065e74b982bf08788899f44a1a2fd1e0
Money <abc12207@gmail.com>
2018-02-20 21:09:14 +0800

    bot emulator demo

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+var restify = require('restify');
 var builder = require('botbuilder');
 
-var connector = new builder.ConsoleConnector().listen();
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s, length: %s", session.message.text,session.message.text.length);
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
 });
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send(" 收到：%s，字串長度：%s", session.message.text, session.message.text.length);
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
